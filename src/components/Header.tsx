60e14451b4d84d96efed519675507f0e63301f63
ryoma-abe <liaozhenanbu387@gmail.com>
2025-06-09 21:13:51 +0900

    Headerコンポーネントに画像を追加し、スクロール位置に応じて背景色を変更する機能を強化しました。また、next.config.tsに新しい画像ドメインを追加しました。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
+"use client";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Header() {
   const [show, setShow] = useState(false);
   useEffect(() => {
     const handleShow = () => {
-      setShow(window.screenY > 100);
+      setShow(window.scrollY > 100);
     };
 
     window.addEventListener("scroll", handleShow);
@@ -13,5 +15,27 @@ export default function Header() {
     };
   }, []);
 
-  return <div className="z-50">HeaderHeaderHeader</div>;
+  return (
+    <div
+      className={`fixed top-0 w-full h-16 p-5 z-10 flex justify-between transition-all ease-in duration-500 ${
+        show ? "bg-black" : ""
+      }`}
+    >
+      <Image
+        className="fixed left-5 w-20 object-contain"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+        width={80}
+        height={30}
+      />
+
+      <Image
+        className="fixed right-5 w-8 object-contain"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="Avatar"
+        width={32}
+        height={32}
+      />
+    </div>
+  );
 }
