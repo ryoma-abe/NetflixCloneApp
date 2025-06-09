bb01bda43422cbad397de605dc7992a16b8db76b
ryoma-abe <liaozhenanbu387@gmail.com>
2025-06-09 21:04:21 +0900

    Headerコンポーネントにスクロール時の表示制御機能を追加しました。スクロール位置に応じてヘッダーの表示を切り替えるためのuseEffectフックを実装しました。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+
 export default function Header() {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const handleShow = () => {
+      setShow(window.screenY > 100);
+    };
+
+    window.addEventListener("scroll", handleShow);
+    return () => {
+      window.removeEventListener("scroll", handleShow);
+    };
+  }, []);
+
   return <div className="z-50">HeaderHeaderHeader</div>;
 }
